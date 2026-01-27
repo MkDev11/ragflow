8b08c067841f818eed9ef9a49930ef709ad75fd8
mkdev11 <YOUR_GITHUB_ID+MkDev11@users.noreply.github.com>
2026-01-27 09:29:44 +0100

    fix: remove redundant columns (Metadata, ID, Timestamp) per reviewer feedback

--- rag/svr/sync_data_source.py
+++ rag/svr/sync_data_source.py
@@ -1191,9 +1191,6 @@ class MySQL(SyncBase):
             database=self.conf.get("database", ""),
             query=self.conf.get("query", ""),
             content_columns=self.conf.get("content_columns", ""),
-            metadata_columns=self.conf.get("metadata_columns", ""),
-            id_column=self.conf.get("id_column", ""),
-            timestamp_column=self.conf.get("timestamp_column", ""),
             batch_size=self.conf.get("batch_size", INDEX_BATCH_SIZE),
         )
 
@@ -1230,9 +1227,6 @@ class PostgreSQL(SyncBase):
             database=self.conf.get("database", ""),
             query=self.conf.get("query", ""),
             content_columns=self.conf.get("content_columns", ""),
-            metadata_columns=self.conf.get("metadata_columns", ""),
-            id_column=self.conf.get("id_column", ""),
-            timestamp_column=self.conf.get("timestamp_column", ""),
             batch_size=self.conf.get("batch_size", INDEX_BATCH_SIZE),
         )
 
